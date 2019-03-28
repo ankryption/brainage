ece491e6a4e96346c38e7075830fce7e5eaa4af6
Ankush Mehta <mail@nksh.io>
2019-03-29 04:39:40 +0530

    Removes redundant files

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
+import Question from '../screens/Question';
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+export default createAppContainer(
+  createSwitchNavigator({
+    Home: Question,
+  })
+);
